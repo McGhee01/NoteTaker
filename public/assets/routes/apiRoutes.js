8a26ad76866d6712ea013b4d58e928e2ded5a4b2
Yah'Mir McGhee <yahmir.mcghee@gmail.com>
2022-02-09 20:42:49 -0500

    helpers folder and fsUtils has been created

---
+++ public/assets/routes/apiRoutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+// GET Route for retrieving all the notes
+router.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+//POST ROUTE
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const { title, text} = req.body;
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully :rocket:`);
+    } else {
+      res.error('Error in adding Note');
+    }
+  });
+//DELETE ROUTE
+router.delete('/:router_id', (req, res) => {
+    const routerId = req.params.router_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make an array of notes except the one with the ID provided in the URL
+        const result = json.filter((router) => router.router_id !== routerId);
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+        // Respond to the DELETE request
+        res.json(`Item ${routerId} has been deleted :wastebasket:`);
+      });
+  });
+module.exports = router;
